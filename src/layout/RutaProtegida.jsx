8b8fce939f2bbeb3ed7539cfb1f4ec36afcb3ceb
thomy2333 <thomyrb30@gmail.com>
2023-11-11 16:19:15 -0300

    Changes to be committed:

--- src/layout/RutaProtegida.jsx
+++ src/layout/RutaProtegida.jsx
@@ -1,5 +1,5 @@
 import { Outlet, Navigate } from "react-router-dom"
-import Header from "../components/header";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 import useAuth from "../hooks/useAuth"
 
